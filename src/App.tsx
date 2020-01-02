54a20403d505b3e2ebc5d2f6b6056f105d0b5cbc
arianna choza <unachoza@gmail.com>
2020-01-01 21:54:41 -0500

    monsters map

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,34 @@ import './App.css';
 
 interface monsters {
   name: string;
-  key: string
+  id: string;
 }
 interface State {
   monstersObj: monsters[];
-  search: string
+  search: string;
 }
-interface Props {
-
-}
+interface Props {}
 class App extends Component<Props, State> {
   constructor(props: Props) {
-    super(props)
+    super(props);
     this.state = {
       monstersObj: [],
-      search: ""
-    }
+      search: '',
+    };
   }
-     async componentDidMount?(): Promise<any> {
-      let result: Response = await fetch("http://jsonplaceholder.typicode.com/users")
-      let users = await result.json()
-      this.setState({monstersObj:users})
-   
+  async componentDidMount?(): Promise<any> {
+    let result: Response = await fetch('http://jsonplaceholder.typicode.com/users');
+    let users = await result.json();
+    this.setState({ monstersObj: users });
+    console.log(this.state.monstersObj);
+  }
 
-  }
-  
   render() {
     return (
       <div className="App">
-        <header className="App-header">Typscript</header>
+        {this.state.monstersObj.map((monster: monsters) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
